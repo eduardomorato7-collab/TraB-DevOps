2f6e48c09fe44902c65c0f58373901afb8745383
eduardomorato7-collab <eduardomorato7@gmail.com>
2026-04-20 07:02:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function soma(a, b) {
+    return a + b;
+}
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+function multiplicacao(a, b) {
+    return a * b;
+}
+
+function divisao(a, b) {
+    return b !== 0 ? a / b : null;
+}
+
+function saudacao(nome) {
+    return "Olá, " + nome;
+}
+
+module.exports = {
+    soma,
+    subtracao,
+    multiplicacao,
+    divisao,
+    saudacao
+};
